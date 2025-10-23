4b8966d1f243e43fa0ac85960f34694ebfb713fc
Kate DeForest <katedeforest02@gmail.com>
2025-10-23 15:59:15 -0600

    Process Registration, Client-side Data Validation, Server-side Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController.js");
 const utilities = require("../utilities/");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -11,6 +12,13 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get(
   "/register",
   utilities.handleErrors(accountController.buildRegister)
+);
+
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
 );
 
 // Export routes
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const utilities = require("./utilities/");
 const session = require("express-session");
 const pool = require("./database/");
 const accountRoute = require("./routes/accountRoute");
+const bodyParser = require("body-parser");
 
 /* ***********************
  * Middleware
@@ -38,6 +39,8 @@ app.use(function (req, res, next) {
   res.locals.messages = require("express-messages")(req, res);
   next();
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 /* ***********************
 View Engine and Templates
