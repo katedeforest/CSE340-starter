77ea973197905d7ffe07707860a885179b949d35
Kate DeForest <katedeforest02@gmail.com>
2025-11-14 20:25:06 -0700

    select inventory items

--- controllers/invController.js
+++ controllers/invController.js
@@ -41,10 +41,12 @@ invCont.buildByInvId = async function (req, res, next) {
  * ************************** */
 invCont.buildManagement = async function (req, res, next) {
   let nav = await utilities.getNav();
+  const classificationList = await utilities.buildClassificationList();
   const grid = await utilities.buildManagementGrid();
   res.render("./inventory/management", {
     title: "Manage Data",
     nav,
+    classificationList,
     grid,
     errors: null,
   });
@@ -159,6 +161,21 @@ invCont.addInventory = async function (req, res) {
       nav,
       errors: null,
     });
+  }
+};
+
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id);
+  const invData = await invModel.getInventoryByClassificationId(
+    classification_id
+  );
+  if (invData[0].inv_id) {
+    return res.json(invData);
+  } else {
+    next(new Error("No data returned"));
   }
 };
 
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -48,4 +48,9 @@ router.post(
   utilities.handleErrors(invController.addInventory)
 );
 
+router.get(
+  "/getInventory/:classification_id",
+  utilities.handleErrors(invController.getInventoryJSON)
+);
+
 module.exports = router;
